3a6e9a7b92da4c33fb4a1e85e97f644dd10263d1
DygDyg <dygdyg@inbox.ru>
2025-05-29 14:53:09 +0300

    2025-05-29 11:53:09

---
+++ GIT_PUSH.bat
@@ -0,0 +1,13 @@
+@echo off
+echo %~dp0
+cd /D %~dp0
+
+for /f "delims=" %%a in ('powershell -Command "(Get-Date).ToUniversalTime().ToString('yyyy-MM-dd HH:mm:ss')"') do set datetime=%%a
+
+echo %datetime%
+echo %datetime% >vers.info
+git add .
+git commit -m "%datetime%"
+git push origin main
+timeout 5
+rem pause
